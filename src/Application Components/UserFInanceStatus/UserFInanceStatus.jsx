9690337b891e86b0208d336bb36b012566e68fa0
FeHunter <hunterfe1111@gmail.com>
2024-02-26 21:29:38 -0300

    Melhorando status do usuário

--- src/Application Components/UserFInanceStatus/UserFInanceStatus.jsx
+++ src/Application Components/UserFInanceStatus/UserFInanceStatus.jsx
@@ -15,7 +15,7 @@ export function UserFInanceStatus ({userName, balance, bills, investments}){
         <div className={style.shrinkOption}>
             <span className={style.item} title="Balance">
                 <i class="fa-solid fa-wallet"></i>
-                <p>Balance: ${balance}</p>
+                <p>Balance: {balance.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
             </span>
             {toggleButton}
         </div>
@@ -24,17 +24,17 @@ export function UserFInanceStatus ({userName, balance, bills, investments}){
     const moreOptions = (
         <div className={style.expandPanel}>
             <span className={style.item} title="Balance">
-                <i class="fa-solid fa-wallet"></i>
-                <p>Balance: ${balance}</p>
-            </span>
-            <span className={style.item}  title="Bills">
-                <i class="fa-solid fa-file-invoice-dollar"></i>
-                <p>Bills: ${bills}</p>
-            </span>
-            <span className={style.item}  title="Investments">
-                <i class="fa-solid fa-money-bill-trend-up"></i>
-                <p>Investments: ${investments}</p>
-            </span> 
+                    <i class="fa-solid fa-wallet"></i>
+                    <p>Balance: {balance.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                </span>
+                <span className={style.item}  title="Bills">
+                    <i class="fa-solid fa-file-invoice-dollar"></i>
+                    <p>Bills: {bills.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                </span>
+                <span className={style.item}  title="Investments">
+                    <i class="fa-solid fa-money-bill-trend-up"></i>
+                    <p>Investments: {investments.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                </span> 
             {toggleButton}
         </div>
     );
